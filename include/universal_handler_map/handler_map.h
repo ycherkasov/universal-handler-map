8e07481a0a1599fc53ca0d57a07e00af5e7609fb
Yuriy Cherkasov <yuri.cherkasov.accounts@gmail.com>
2016-07-10 15:15:17 +0700

    complete static version

--- include/universal_handler_map/handler_map.h
+++ include/universal_handler_map/handler_map.h
@@ -1,12 +1,18 @@
 #pragma once
 #include <functional>
 
-
+/// @param MapName - name of (int-function) compile-time map 
+/// @param Base - std::function<> template with appropriate signature
+/// @example DeclareUniversalMap(OneParam, std::function<void(int)>);
 #define DeclareUniversalMap(MapName, Base) using BaseFunc##MapName = Base; \
 template <int Val> \
 struct UniversalMap##MapName {};
 
 
+/// @param MapName - name of (int-function) compile-time map 
+/// @param Value - integer of converted to integer value 
+/// @param Handler - std::function or convertable with appropriate signature
+/// @example: InsertUniversalMap(OneParam, 0, &MyHandlers1::class_handler_zero);
 #define InsertUniversalMap(MapName, Value, Handler) template <> \
 struct UniversalMap##MapName<Value> : BaseFunc##MapName \
 { \
@@ -20,5 +26,7 @@ struct UniversalMap##MapName<Value> : BaseFunc##MapName \
 }; \
 UniversalMap##MapName<Value>::Function UniversalMap##MapName<Value>::handler##Value = Handler;
 
+/// @param MapName - name of (int-function) compile-time map 
+/// @param Value - integer which is call mapped handler
 #define UniversalMapCall(MapName, Value) UniversalMap##MapName<Value>
 
--- test/func_tests/func_tests.cpp
+++ test/func_tests/func_tests.cpp
@@ -1,5 +1,4 @@
-#include <thread>
-#include <numeric>
+#include <stack>
 #include <iostream>
 #include <universal_handler_map/handler_map.h>
 
@@ -10,20 +9,135 @@
 using namespace boost::unit_test;
  
 // Functional tests
- 
+
+// Push test values so that take back in reverse order
+class StackChecker
+{
+public:
+    
+    static std::stack<std::string>& instance()
+    {
+        return checker_stack_;
+    }
+
+private:
+    static std::stack<std::string> checker_stack_;
+};
+std::stack<std::string> StackChecker::checker_stack_;
+
 BOOST_AUTO_TEST_SUITE(HandlerMapFunctionalTests);
  
+void zero_handler()
+{
+    StackChecker::instance().push("zero_handler()");
+}
 
-// Test concurrent push/pop
+void one_handler()
+{
+    StackChecker::instance().push("one_handler()");
+}
+
+class MyHandlers
+{
+public:
+    static void class_handler_two()
+    {
+        StackChecker::instance().push("MyHandlers::class_handler_two()");
+    }
+    static void class_handler_three()
+    {
+        StackChecker::instance().push("MyHandlers::class_handler_three()");
+    }
+};
+
+// Map 0-params handlers to integer values
+DeclareUniversalMap(NonParam, std::function<void()>);
+InsertUniversalMap(NonParam, 0, &zero_handler);
+InsertUniversalMap(NonParam, 1, &one_handler);
+InsertUniversalMap(NonParam, 2, &MyHandlers::class_handler_two);
+InsertUniversalMap(NonParam, 3, &MyHandlers::class_handler_three);
+
 BOOST_AUTO_TEST_CASE(Zero_Params_Test)
 {
 	std::cout << "Zero params test..." << std::endl;
-	//testValuesConcurrently<int>();
-	//testValuesConcurrently<long long>();
-	//testValuesConcurrently<double>();
-	//testValuesConcurrently<void*>();
+
+    // Call 0-params handlers
+    UniversalMapCall(NonParam, 0)::call();
+    UniversalMapCall(NonParam, 1)::call();
+    UniversalMapCall(NonParam, 2)::call();
+    UniversalMapCall(NonParam, 3)::call();
+
+    BOOST_CHECK_EQUAL(StackChecker::instance().top(), std::string("MyHandlers::class_handler_three()"));
+    StackChecker::instance().pop();
+    BOOST_CHECK_EQUAL(StackChecker::instance().top(), std::string("MyHandlers::class_handler_two()"));
+    StackChecker::instance().pop();
+    BOOST_CHECK_EQUAL(StackChecker::instance().top(), std::string("one_handler()"));
+    StackChecker::instance().pop();
+    BOOST_CHECK_EQUAL(StackChecker::instance().top(), std::string("zero_handler()"));
+    StackChecker::instance().pop();
+    BOOST_CHECK_EQUAL(StackChecker::instance().empty(), true);
 }
 
- 
+void zero_handler1(int i)
+{
+    std::stringstream s;
+    s << "zero_handler1(" << i << ")";
+    StackChecker::instance().push(s.str());
+}
+
+void one_handler1(int i)
+{
+    std::stringstream s;
+    s << "one_handler1(" << i << ")";
+    StackChecker::instance().push(s.str());
+}
+
+
+class MyHandlers1
+{
+public:
+    static void class_handler_two(int i)
+    {
+        std::stringstream s;
+        s << "MyHandlers1::class_handler_two(" << i << ")";
+        StackChecker::instance().push(s.str());
+    }
+    static void class_handler_three(int i)
+    {
+        std::stringstream s;
+        s << "MyHandlers1::class_handler_three(" << i << ")";
+        StackChecker::instance().push(s.str());
+    }
+};
+
+// Map 1-params handlers to integer values
+DeclareUniversalMap(OneParam, std::function<void(int)>);
+InsertUniversalMap(OneParam, 0, &zero_handler1);
+InsertUniversalMap(OneParam, 1, &one_handler1);
+InsertUniversalMap(OneParam, 2, &MyHandlers1::class_handler_two);
+InsertUniversalMap(OneParam, 3, &MyHandlers1::class_handler_three);
+
+BOOST_AUTO_TEST_CASE(One_Params_Test)
+{
+    std::cout << "One params test..." << std::endl;
+
+    // Call 1-params handlers
+    UniversalMapCall(OneParam, 0)::call(0);
+    UniversalMapCall(OneParam, 1)::call(1);
+    UniversalMapCall(OneParam, 2)::call(2);
+    UniversalMapCall(OneParam, 3)::call(3);
+
+    BOOST_CHECK_EQUAL(StackChecker::instance().top(), std::string("MyHandlers1::class_handler_three(3)"));
+    StackChecker::instance().pop();
+    BOOST_CHECK_EQUAL(StackChecker::instance().top(), std::string("MyHandlers1::class_handler_two(2)"));
+    StackChecker::instance().pop();
+    BOOST_CHECK_EQUAL(StackChecker::instance().top(), std::string("one_handler1(1)"));
+    StackChecker::instance().pop();
+    BOOST_CHECK_EQUAL(StackChecker::instance().top(), std::string("zero_handler1(0)"));
+    StackChecker::instance().pop();
+    BOOST_CHECK_EQUAL(StackChecker::instance().empty(), true);
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
  
